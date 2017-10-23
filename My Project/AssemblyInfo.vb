7d35f98394654f5ca99e6778caf822a8ef427732
Tom Flídr <tomflidr@gmail.com>
2017-10-23 17:25:12 +0200

    VS2017

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
@@ -21,18 +21,18 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("d7594b0e-a7ac-4a77-a22c-af8d73894446")>
+<Assembly: Guid("d9536bf9-e3bf-4e5c-883f-24c965c6bc87")>
 
 ' Version information for an assembly consists of the following four values:
 '
-'   Major Version
-'   Minor Version 
-'   Build Number
-'   Revision
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.2.5.0")>
 <Assembly: AssemblyFileVersion("1.2.5.0")>
